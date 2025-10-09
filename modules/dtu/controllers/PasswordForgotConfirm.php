a2158240166408d53141e026b990ade8e523d54e
Noiret Maxime <maxime.noiret@etu.univ-amu.fr>
2025-10-09 15:28:48 +0200

    Add stylesheet to ForgotPasswordView rendering

--- modules/dtu/controllers/PasswordForgotConfirm.php
+++ modules/dtu/controllers/PasswordForgotConfirm.php
@@ -10,9 +10,10 @@ class PasswordForgotConfirm implements Controller {
 
   const string PATH = '/user/forgot';
   const string METH = 'POST';
+  const string STYLESHEET = DIRECTORY_SEPARATOR . '_assets' . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR . 'style.css';
 
   function control(): void {
-    echo new ForgotPasswordView(Account::forgotPassword($_POST['email']))->render();
+    echo new ForgotPasswordView(Account::forgotPassword($_POST['email']))->render('Forgot Password - DealTonBUT', self::STYLESHEET);
   }
 
   static function resolve(string $path, string $meth): bool {
