c6a3815d9431bab74d14f5395e62a0342b181947
Baurens-Torrente Valentin <valentin.baurens-torrente@etu.univ-amu.fr>
2025-10-15 14:56:43 +0200

    made it ready for log in use.

--- modules/dtu/controllers/Account.php
+++ modules/dtu/controllers/Account.php
@@ -15,13 +15,13 @@ class Account implements Controller
 
     function control(): void
     {
-//        if (!isset($_SESSION['logged-in']) || $_SESSION['logged-in'] !== true) {
-//            echo (new LoginFormView())->render("Login - DealTonBUT", self::STYLESHEET);
-//        } else {
-//            header('Location: /account');
-//        }
+        if (!isset($_SESSION['logged-in']) || $_SESSION['logged-in'] !== true) {
+            echo (new LoginFormView())->render("Login - DealTonBUT", self::STYLESHEET);
+        } else {
+            header('Location: /account');
+        }
       // TEMPORARY (just for CSS)
-      echo (new AccountPageView())->render('Account - DealTonBUT', self::STYLESHEET);
+      //echo (new AccountPageView())->render('Account - DealTonBUT', self::STYLESHEET);
     }
 
     static function resolve(string $path, string $meth): bool
