6341ba094ddd74a5b1e4f39f255ca1f510d35652
Pablo <pablo.sene@etu.univ-amu.fr>
2025-10-15 16:50:20 +0200

    Ajout de la navbar

--- modules/dtu/controllers/MarketPlace.php
+++ modules/dtu/controllers/MarketPlace.php
@@ -10,18 +10,27 @@ class MarketPlace implements Controller {
   private const PATH = '/marketplace';
   private const METH = 'GET';
 
+<<<<<<< HEAD
   const string STYLESHEET = DIRECTORY_SEPARATOR . '_assets' . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR . 'Account.css';
+=======
+  private const STYLESHEET = DIRECTORY_SEPARATOR . '_assets' . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR . 'style.css';
+>>>>>>> 7bc9c5c (Ajout de la navbar)
   
   function control(): void {
     if (!isset($_SESSION['logged-in']) || $_SESSION['logged-in'] !== true) {
       header('Location: /user/login');
     } else {
+<<<<<<< HEAD
       echo (new MarketPlaceView())->render("Place de Marché - DealTonBUT", self::STYLESHEET);
     }
+=======
+      echo (new MarketPlaceView())->render("Place de Marché - DealTonBUT", static::STYLESHEET);
+>>>>>>> 7bc9c5c (Ajout de la navbar)
   } 
+  }
 
-  static function resolve(string $path, string $meth): bool {
-    return $path === self::PATH && $meth === self::METH;
+  public static function resolve(string $path, string $meth): bool {
+    return $path === static::PATH && $meth === static::METH;
   }
 
 
