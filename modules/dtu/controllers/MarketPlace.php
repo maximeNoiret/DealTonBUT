6d84258c7a98aa1cf651b89f5a9db1c94216321c
Pablo <pablo.sene@etu.univ-amu.fr>
2025-10-20 14:17:48 +0200

    Fix: Enlevage du code répliquer

--- modules/dtu/controllers/MarketPlace.php
+++ modules/dtu/controllers/MarketPlace.php
@@ -11,8 +11,6 @@ class MarketPlace implements Controller {
   private const METH = 'GET';
   private const STYLESHEET = DIRECTORY_SEPARATOR . '_assets' . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR . 'style.css';
 
-  private const STYLESHEET = DIRECTORY_SEPARATOR . '_assets' . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR . 'style.css';
-  
   function control(): void {
     if (!isset($_SESSION['logged-in']) || $_SESSION['logged-in'] !== true) {
       header('Location: /user/login');
@@ -20,9 +18,6 @@ class MarketPlace implements Controller {
       echo (new MarketPlaceView())->render("Place de Marché - DealTonBUT", static::STYLESHEET);
     }
   } 
-      echo (new MarketPlaceView())->render("Place de Marché - DealTonBUT", static::STYLESHEET);
-  } 
-  }
 
   public static function resolve(string $path, string $meth): bool {
     return $path === static::PATH && $meth === static::METH;
