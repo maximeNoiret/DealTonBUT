9a8d7699c560942b320c0b082a32d715c00794f5
DEMANGE Martin <alextrav.12354@gmail.com>
2025-10-21 09:44:50 +0200

    fix : in the call of the function header in AbstractSubView.php

--- modules/core/views/AbstractSubView.php
+++ modules/core/views/AbstractSubView.php
@@ -5,7 +5,7 @@ namespace core\views;
 use core\views\AbstractView;
 
 abstract class AbstractSubView extends AbstractView {
-  public function header(string $sectionType, string $sectionClass, string $customvalue = ''): string {
+  public function header(string $sectionType, string|array $sectionClass, string $customvalue = ''): string {
     return '<' . $sectionType . ' class="' . $sectionClass . '">' . "\n";
   }
 
@@ -13,7 +13,7 @@ abstract class AbstractSubView extends AbstractView {
     return '</' . $this->sectionType . '>';
   }
 
-  function render(string $sectionType, string $sectionClass): string {
+  function render(string $sectionType, string|array $sectionClass): string {
     $this->sectionType = $sectionType;
     return $this->header($sectionType, $sectionClass) . $this->body() . $this->footer();
   }
