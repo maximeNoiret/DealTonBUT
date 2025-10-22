862fd9eafc62d4cfa53a8c835c2f266a45d79164
Baurens-Torrente Valentin <valentin.baurens-torrente@etu.univ-amu.fr>
2025-10-22 11:43:50 +0200

    fix: made offer unable to be accessed without logged in

--- modules/dtu/controllers/Trade/AddOffer/AddOffer.php
+++ modules/dtu/controllers/Trade/AddOffer/AddOffer.php
@@ -17,7 +17,11 @@ class AddOffer{
 
     function control(): void
     {
+      if (!isset($_SESSION['logged-in']) || $_SESSION['logged-in'] !== true) {
+        header('Location: /user/login');
+      } else {
         echo (new AddOfferView(''))->render("AddOffer - DealTonBUT", self::STYLESHEET);
+      }
     }
 
     static function resolve(string $path, string $meth): bool
