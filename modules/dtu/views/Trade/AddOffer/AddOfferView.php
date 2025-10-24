c19ee4c4b523f6560ca8389e91779f9791127670
DEMANGE Martin <alextrav.12354@gmail.com>
2025-10-24 16:13:14 +0200

    docs : made the doc for the class AddOfferView.php

--- modules/dtu/views/Trade/AddOffer/AddOfferView.php
+++ modules/dtu/views/Trade/AddOffer/AddOfferView.php
@@ -5,14 +5,26 @@ namespace views\Trade\AddOffer;
 use core\views\AbstractView;
 
 class AddOfferView extends AbstractView{
+  /**
+   * @description Constructor of the class
+   * @param string $offresHtml :
+   */
     public function __construct(private string $offresHtml)
     {
     }
 
+  /**
+   * @description Method that give the path tp the corresponding .html
+   * @return string
+   */
     function path(): string {
         return __DIR__ . DIRECTORY_SEPARATOR . 'AddOffer.html';
     }
 
+  /**
+   * @description Replace keys value by their real value in the associated .html file
+   * @return array|mixed[]
+   */
     function templateValues(): array {
         return [
             'OFFRES' => $this->offresHtml,
@@ -25,6 +37,10 @@ class AddOfferView extends AbstractView{
         ];
     }
 
+  /**
+   * @description Contain the title of the page, that will be shown on the navbar
+   * @return string
+   */
   function navbarText(): string
   {
     return 'Creer une offre';
