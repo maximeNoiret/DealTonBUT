1c4a9ab389618d407966be3e5375caf63f6099d6
DEMANGE Martin <alextrav.12354@gmail.com>
2025-10-24 16:01:46 +0200

    docs : made the doc for the class SettingsPageView.php

--- modules/dtu/views/User/SettingsPage/SettingsPageView.php
+++ modules/dtu/views/User/SettingsPage/SettingsPageView.php
@@ -5,13 +5,17 @@ use core\views\AbstractView;
 
 class SettingsPageView extends AbstractView
 {
-  private static self $instance;
+  /**
+   * @description Method that give the path tp the corresponding .html
+   * @return string
+   */
   function path(): string
   {
     return __DIR__ . DIRECTORY_SEPARATOR . 'SettingsPage.html';
   }
 
   /**
+   * @description Replace keys value by their real value in the associated .html file
    * @return array<string,mixed>
    */
   public function templateValues(): array
@@ -19,13 +23,10 @@ class SettingsPageView extends AbstractView
     return [];
   }
 
-  public static function getInstance(): self {
-    if (!isset(self::$instance)) {
-      self::$instance = new self();
-    }
-    return self::$instance;
-  }
-
+  /**
+   * @description Contain the title of the page, that will be shown on the navbar
+   * @return string
+   */
   function navbarText(): string
   {
     return 'Settings';
