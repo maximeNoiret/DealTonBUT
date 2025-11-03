b2881c82b214ea9f1e30340c8ded530783b25931
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2025-11-03 16:46:13 +0100

    feat: Added better message for password reset email

--- modules/core/models/Mailer.php
+++ modules/core/models/Mailer.php
@@ -14,6 +14,9 @@ require 'vendor/autoload.php';
 
 
 class Mailer {
+  /**
+   * @return string : Returns the API key stored in the .apkey file.
+   */
     public static function getApiKey(): string {
         $file = fopen(__DIR__ . '/../../../.apkey', 'r') or die('File didn\'t open.');
         $apiKey = fgets($file);
@@ -21,7 +24,17 @@ class Mailer {
         return rtrim($apiKey);
     }
 
-    public static function sendMail(string $fromUsername, string $toAddress, string $toName, string $subject, string $body, bool $isHTML): bool {
+  /**
+   * @param string $fromUsername : Username part of the from address.
+   * @param string $toAddress : Recipient email address.
+   * @param string $toName : Recipient name.
+   * @param string $subject : Email subject.
+   * @param string $body : Email body.
+   * @param string $altBody : Alternative email body for non-HTML email clients.
+   * @param bool $isHTML : Indicates if the email body is in HTML format.
+   * @return bool : Returns true if mail sent successfully, false otherwise.
+   */
+    public static function sendMail(string $fromUsername, string $toAddress, string $toName, string $subject, string $body, string $altBody, bool $isHTML): bool {
         $mail = new PHPMailer(true);
 
         try {
@@ -43,7 +56,7 @@ class Mailer {
             $mail->isHTML($isHTML);
             $mail->Subject = $subject;
             $mail->Body    = $body;
-            $mail->AltBody = $body;
+            $mail->AltBody = $altBody;
 
             $mail->send();
             return true;
@@ -59,10 +72,20 @@ class Mailer {
      * @param $link  : Reset link.
      */
     public static function sendForgotPassword(string $email, string $link): bool {
-        $body = '<h1>Forgot Password</h1>
-<p>You have forgotten your password. Please click <a href="' . $link . '">HERE</a> to reset your password.</p>';
-        $name = 'REPLACE ME WITH FUNCTION CALL';
+        $body = '<h1>DealTonBUT - Mot de passe oublié</h1>
+<p>Vous recevez ce mail suite à une requête de réinitialisation de mot de passe sur DealTonBUT.</p>
+<p>Veuillez cliquer <a href="' . $link . '">ICI</a> afin de réinitialiser votre mot de passe.</p><br>
+<p>Si vous ne pouvez pas cliquer sur le lien, copiez collez celui-ci:<p>
+<p style="color: #0077FF">' . $link . '</p><br><br>
+<p>Si vous n\'avez pas fait cette requête, vous pouvez ignorer ce mail.</p><br>
+<h5>DealTonBUT - On n\'a pas encore de slogan.</h5>';
+        $altBody = 'DealTonBUT - Mot de passe oublié\n\n' .
+            'Vous recevez ce mail suite à une requête de réinitialisation de mot de passe sur DealTonBUT.\n' .
+            'Veuillez utiliser ce lien:\n ' . $link . '\n\n' .
+            'Si vous n\'avez pas fait cette requête, vous pouvez ignorer ce mail.\n\n' .
+            'DealTonBUT - On n\'a pas encore de slogan.';
+        $name = 'REPLACE ME WITH FUNCTION CALL';  // TODO: make a function to get name from email and use it here
 
-        return self::sendMail('no-reply', $email, $name, 'Forgot Password', $body, true);
+        return self::sendMail('no-reply', $email, $name, 'Forgot Password', $body, $altBody, true);
     }
 }
