c8bfee0ed551261ecdac333b05b13b91839272d8
noroi28 <aline.dmnt83@gmail.com>
2025-11-05 13:52:59 +0100

    fix : css asaptif pour la page main login et sigin avec en plus modifiaction de hindex pour que le css de tel fonctionne

--- modules/core/views/AbstractView.php
+++ modules/core/views/AbstractView.php
@@ -27,6 +27,7 @@ abstract class AbstractView {
     <link rel="icon" type="image/x-icon" href="/_assets/images/favicon.ico">
     <link rel="shortcut icon" type="image/x-icon" href="/_assets/images/favicon.ico">
     <link rel="icon" type="image/png" href="/_assets/images/favicon.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     ' . $stylesheetsHtml . '
   </head>
   <body>
