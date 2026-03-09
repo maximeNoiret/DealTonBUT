3145934b01d35414d6409f258b5ff78c790ba218
DEMANGE Martin <alextrav.12354@gmail.com>
2026-03-09 20:32:26 +0100

    feat : added a check to impede the deletion of admin account in the admin panel

--- modules/dtu/controllers/User/AccountPage/DeleteAccount.php
+++ modules/dtu/controllers/User/AccountPage/DeleteAccount.php
@@ -12,10 +12,34 @@ class DeleteAccount implements Controller
   const string PATH = '/user/delete-account';
   const string METH = 'POST';
 
+  /**
+   * @description Check if the user is logged in, if not redirect to the login page.
+   * If yes, check if the user is an admin and if the email to delete is in $_POST['remove-account'],
+   * if yes delete the account with the email in $_POST['remove-account'].
+   * If not redirect to the login page, if the user is not an admin delete the account
+   * of the logged-in user and redirect to the login page
+   * @return void
+   */
   function control(): void {
     if (!isset($_SESSION['logged-in']) || $_SESSION['logged-in'] !== true) {
       header('Location: /user/login');
       exit;
+    }
+
+    if (AccountDB::getInstance()->isAdmin($_SESSION['email'])){
+      if (isset($_POST['remove-account']) && is_string($_POST['remove-account'])) {
+        $email = $_POST['remove-account'];
+        if (AccountDB::getInstance()->isAdmin($email)) {
+          header('Location: /admin');
+          exit;
+        }
+        AccountDB::getInstance()->deleteUser($email);
+        header('Location: /admin');
+        exit;
+      } else {
+        header('Location: /login');
+        exit;
+      }
     }
 
     $email = '';
