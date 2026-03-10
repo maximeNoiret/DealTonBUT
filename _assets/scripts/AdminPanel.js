b2417e3a6ded9ae061d5801bbcdc521ec7e5221e
DEMANGE Martin <alextrav.12354@gmail.com>
2026-03-10 15:21:14 +0100

    refactor : moved the js script for the admin panel in a specialised .js file in /_asset/scripts/AdminPanel.js

---
+++ _assets/scripts/AdminPanel.js
@@ -0,0 +1,43 @@
+// scrip for switch between the different admin panel options (users, offers, transactions).
+// Used in views/User/AdminPanel/AdminPanel.html
+document.addEventListener('DOMContentLoaded', function (){
+    // Select the panels, by using their CSS class name
+    const adminAccount = document.querySelector('.manage-account-panel');
+    const adminOffer = document.querySelector('.manage-offer-panel');
+    const adminTransaction = document.querySelector('.manage-transaction-panel');
+    // Select the buttons, by using their id
+    const userButton = document.querySelector('#account-button');
+    const offerButton = document.querySelector('#offer-button');
+    const transactionButton = document.querySelector('#transaction-button');
+
+    // Function that show the selected panel and hide the others
+    /**
+     * Set the display of adminAccount to 'flex' and the others to 'none'
+     */
+    function showAccounts() {
+        adminAccount.style.display = 'flex';
+        adminOffer.style.display = 'none';
+        adminTransaction.style.display = 'none';
+    }
+    /**
+     * Set the display of adminOffer to 'flex' and the others to 'none'
+     */
+    function showOffers() {
+        adminAccount.style.display = 'none';
+        adminOffer.style.display = 'flex';
+        adminTransaction.style.display = 'none';
+    }
+    /**
+     * Set the display of adminTransaction to 'flex' and the others to 'none'
+     */
+    function showTransactions() {
+        adminAccount.style.display = 'none';
+        adminOffer.style.display = 'none';
+        adminTransaction.style.display = 'flex';
+    }
+
+    // Add event listeners to the buttons
+    userButton.addEventListener('click', showAccounts);
+    offerButton.addEventListener('click', showOffers);
+    transactionButton.addEventListener('click', showTransactions);
+})
