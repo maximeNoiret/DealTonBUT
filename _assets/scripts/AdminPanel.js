16e64a545f807b1189ace79742759cd41c95b3dc
DEMANGE Martin <alextrav.12354@gmail.com>
2026-03-10 20:15:55 +0100

    misc : scrapped the panel that would allow the display of the transaction

--- _assets/scripts/AdminPanel.js
+++ _assets/scripts/AdminPanel.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', function (){
     // Select the panels, by using their CSS class name
     const adminAccount = document.querySelector('.manage-account-panel');
     const adminOffer = document.querySelector('.manage-offer-panel');
-    const adminTransaction = document.querySelector('.manage-transaction-panel');
+    // SCRAPPED
+    // const adminTransaction = document.querySelector('.manage-transaction-panel');
+
     // Select the buttons, by using their id
     const userButton = document.querySelector('#account-button');
     const offerButton = document.querySelector('#offer-button');
-    const transactionButton = document.querySelector('#transaction-button');
+    // SCRAPPED
+    // const transactionButton = document.querySelector('#transaction-button');
 
     // Function that show the selected panel and hide the others
     /**
@@ -17,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function (){
     function showAccounts() {
         adminAccount.style.display = 'flex';
         adminOffer.style.display = 'none';
-        adminTransaction.style.display = 'none';
+        // adminTransaction.style.display = 'none';
     }
     /**
      * Set the display of adminOffer to 'flex' and the others to 'none'
@@ -25,21 +28,24 @@ document.addEventListener('DOMContentLoaded', function (){
     function showOffers() {
         adminAccount.style.display = 'none';
         adminOffer.style.display = 'flex';
-        adminTransaction.style.display = 'none';
+        // adminTransaction.style.display = 'none';
     }
-    /**
-     * Set the display of adminTransaction to 'flex' and the others to 'none'
-     */
-    function showTransactions() {
-        adminAccount.style.display = 'none';
-        adminOffer.style.display = 'none';
-        adminTransaction.style.display = 'flex';
-    }
+
+    // SCRAPPED
+    // /**
+    //  * Set the display of adminTransaction to 'flex' and the others to 'none'
+    //  */
+    // function showTransactions() {
+    //     adminAccount.style.display = 'none';
+    //     adminOffer.style.display = 'none';
+    //     adminTransaction.style.display = 'flex';
+    // }
 
     // Add event listeners to the buttons
     userButton.addEventListener('click', showAccounts);
     offerButton.addEventListener('click', showOffers);
-    transactionButton.addEventListener('click', showTransactions);
+    // SCRAPPED
+    // transactionButton.addEventListener('click', showTransactions);
 
     // by default the accounts panel is shown
     showAccounts();
